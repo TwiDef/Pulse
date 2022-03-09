c4e6b1161085e7c826e78a75fa267344c9875600
deferenseo <deferenseo@gmail.com>
2022-03-09 15:51:19 +0300

    added slider

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 500,
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrows/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrows/right.svg"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
