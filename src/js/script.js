e5031d31aef1037438b94701512d5eebc5d03fb1
Deferenseo <Deferenseo@gmail.com>
2022-03-25 17:10:56 +0300

    adaptive catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,6 @@ document.querySelector('.next').addEventListener('click', function() {
                 .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
         });
 
-
         // табы карточек 
         function toggleSlide(item) {
             $(item).each(function(i) {
