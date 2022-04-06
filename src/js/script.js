d29b291d9c3b32652531fcc04ec63387aa1c06ea
Def <deferenseo@gmail.com>
2022-04-06 15:39:26 +0300

    added email post

--- src/js/script.js
+++ src/js/script.js
@@ -103,6 +103,27 @@ document.querySelector('.next').addEventListener('click', function() {
         validateForms('#consultation-form');
         validateForms('#consultation form');
         validateForms('#order form');
+
+        $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+        // отправка сообщений
+        $('form').submit(function(e) {
+            e.preventDefault();
+            if (!$(this).valid()) {
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "mailer/smart.php",
+                data: $(this).serialize()
+            }).done(function() {
+                $(this).find("input").val("");
+                $('#consultation, #order').fadeOut();
+                $('.overlay, #thanks').fadeIn('slow');
+                $('form').trigger('reset');
+            });
+            return false;
+        });
     });
 })(jQuery);
 
